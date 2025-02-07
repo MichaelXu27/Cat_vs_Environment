cd701c32670daf1d83d347b53114d5ecbff21b40
dren02 <d.ren@wustl.edu>
2025-02-07 15:26:53 -0600

    Add left and right movement

--- Assets/Code/Movement.cs
+++ Assets/Code/Movement.cs
@@ -10,6 +10,8 @@ public class Movement : MonoBehaviour
     public float groundCheckRadius;
     public LayerMask groundLayer;
     private bool isTouchingGround;
+    private float direction = 0f;
+    public float speed = 5f;
 
     // Use this for initialization
     void Start () 
@@ -19,9 +21,23 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update() 
     { 
+        // jump
         isTouchingGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         if (isTouchingGround && Input.GetButtonDown("Jump")) {
             player.velocity = new Vector2(player.velocity.x, jumpSpeed); 
+        }
+        // move left & right
+        direction = Input.GetAxis("Horizontal");
+        if (direction > 0f) {
+            player.velocity = new Vector2(direction * speed, player.velocity.y);
+        }
+        else if (direction < 0f)
+        {
+            player.velocity = new Vector2(direction * speed, player.velocity.y);
+        }
+        else
+        {
+            player.velocity = new Vector2(0, player.velocity.y);
         }
     }
 }
