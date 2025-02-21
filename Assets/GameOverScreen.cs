e0662eff62609db78a54fde7ba7008b7559e499a
Emily Han <emily.han@wustl.edu>
2025-02-21 01:08:31 -0600

    game over script
    game over script

---
+++ Assets/GameOverScreen.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScreen : MonoBehaviour
+{
+    public Text pointsText;
+
+    public void Setup(int score)
+    {
+        gameObject.SetActive(true);
+        pointsText.text = score.ToString() + " POINTS";
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ExitButton()
+    {
+        SceneManager.LoadScene("InitialScene");
+    }
+}
+
